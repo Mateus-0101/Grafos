98e5a2f6e9e7cb1acfbeef5530354c2cc0f3d933
Mateus-0101 <mateusantunes2342@gmail.com>
2022-09-12 17:11:17 -0300

    modified:   bishu.c

--- bishu.c
+++ bishu.c
@@ -5,48 +5,59 @@ typedef struct vertice
 {
     int visitado;
     int lista_adj[1000];
-    int tam_lista_adj;
+    int tamanho;
     int distancia;
 }vertice;
 
+void DFS(vertice *v, int qtd_vertices, int raiz, int distancia)
+{
+    if (v[raiz].visitado != 0)
+    {
+        return;
+    }
 
-void mostrar_tudo (vertice * vertices, int qtd_vertices);
-void dfs(vertice * vertices, int qtd_vertices, int raiz,int distancia);
+    v[raiz].visitado = 1;
+    v[raiz].distancia = distancia;
+
+    for(int i = 0; i < v[raiz].tamanho; i++)
+    {
+        if (v[v[raiz].lista_adj[i]].visitado == 0)
+        {
+            DFS(v,qtd_vertices,v[raiz].lista_adj[i],(distancia+1));
+        }
+    }
+}
 
 int main()
 {
-    int qtd_vertices, qtd_arestas;
-    int i,a,b,qtd_cc=0;
-    vertice * vertices;
-    scanf("%d",&qtd_vertices);
+    int qtd_vertices, qtd_arestas, qtd_garotas, u, v;
     int garotas[1000];
-    int qtd_garotas;
 
-    // printf("\n Quantidade de vertices: %d ",qtd_vertices);
-    // printf("\n Quantidade de arestas: %d\n",qtd_arestas);
+    scanf("%d",&qtd_vertices);
 
-    vertices = (vertice*)calloc(qtd_vertices+1,sizeof(vertice));
+    vertice *vertices = (vertice*)calloc(qtd_vertices+1,sizeof(vertice));
 
-    for(i=0;i<qtd_vertices-1;i++)
+    for(int i = 0; i < (qtd_vertices-1); i++)
     {
-        scanf("%d %d",&a,&b);
-        vertices[a].lista_adj[vertices[a].tam_lista_adj] = b;
-        vertices[a].tam_lista_adj++;
-        vertices[b].lista_adj[vertices[b].tam_lista_adj] = a;
-        vertices[b].tam_lista_adj++;
+        scanf("%d %d",&u,&v);
+        vertices[u].lista_adj[vertices[u].tamanho] = v;
+        vertices[u].tamanho++;
+        vertices[v].lista_adj[vertices[v].tamanho] = u;
+        vertices[v].tamanho++;
     }
 
-    dfs(vertices,qtd_vertices,1,0);
+    DFS(vertices,qtd_vertices,1,0);
 
     scanf("%d",&qtd_garotas);
 
-    for(int i=0;i<qtd_garotas;i++)
+    for(int i = 0; i < qtd_garotas; i++)
     {
         scanf("%d",&garotas[i]);
     }
 
-    int id_escolhida=garotas[0];
-    for(int i=1;i<qtd_garotas;i++)
+    int id_escolhida = garotas[0];
+
+    for(int i = 1; i < qtd_garotas; i++)
     {
         if (vertices[garotas[i]].distancia < vertices[id_escolhida].distancia)
         {
@@ -64,57 +75,7 @@ int main()
         }
     }
 
-    printf("\n Id escolhida: %d\n",id_escolhida);
-
-
-
-    // mostrar_tudo(vertices,qtd_vertices);
-
-    // for(i=1;i<=qtd_vertices;i++)
-    // {
-    //     printf("\n Distancia até %d = %d",i,vertices[i].distancia);
-    // }
-
-    // printf("\n%d",qtd_cc);
-
+    printf("\nId escolhida: %d\n",id_escolhida);
 
     return 0;
-}
-
-
-void mostrar_tudo (vertice * vertices, int qtd_vertices)
-{
-    int i,j;
-
-    for(i=1;i<=qtd_vertices;i++)
-    {
-        printf("\n Vertice: %d",i);
-        printf("\n Lista de adjacencias: ");
-
-        for(j=0;j<vertices[i].tam_lista_adj;j++)
-        {
-            printf("%d ",vertices[i].lista_adj[j]);
-        }
-    }
-}
-
-void dfs(vertice * vertices, int qtd_vertices, int raiz,int distancia)
-{
-    int i;
-    if (vertices[raiz].visitado!=0)
-    {
-        return;
-    }
-
-    // printf("\n %d",raiz);
-    vertices[raiz].visitado=1;
-    vertices[raiz].distancia = distancia;
-
-    for(i=0;i<vertices[raiz].tam_lista_adj;i++)
-    {
-        if (vertices[vertices[raiz].lista_adj[i]].visitado==0)
-        {
-            dfs(vertices,qtd_vertices,vertices[raiz].lista_adj[i],distancia+1);
-        }
-    }
 }
